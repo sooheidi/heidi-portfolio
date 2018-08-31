06486a4bed40b7a676b871cd33ae56d19be86df4
Heidi Soo <sooheidi@gmail.com>
2018-08-31 14:16:33 -0700

    Add first placeholder page for 'Code'

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Menu } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Code from './pages/Code.js';
 
 class App extends Component {
   render() {
     return (
-      <Menu inverted>
-        <Menu.Item name='home'/>
-        <Menu.Item name='Code'/>
-        <Menu.Item name='Art'/>
-        <Menu.Item name='Photography'/>
-        <Menu.Item name='Recipes'/>
+      <Router>
+        <div>
+          <Menu inverted>
+            <Link to='/' className='item'>Home</Link>
+            <Link to='/code' className='item'>Code</Link>
+            <Link to='/photography' className='item'>Photography</Link>
+            <Link to='/art' className='item'>Art</Link>
+            <Link to='/recipes' className='item'>Recipes</Link>
+          </Menu>
+          <Route path='/' exact strict render={() =>
+            <h1> Home!!</h1>
+          }/>
 
-      </Menu>
+          <Route path='/code' exact component={Code}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Code.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Code.css';
+
+class Code extends Component {
+  render() {
+    return (
+      <h1>Hi! I'm the code page!</h1>
+    );
+  }
+}
+
+export default Code;
